b7d9459a2cb19e58282aec02e24cfaca0422d678
UdayKiran0802 <udaykiran0802.com>
2021-09-01 16:49:45 +0000

    authentcation-route

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <Link to="/" className="nav-link">
+      {' '}
+      <p className="nav-text">Home</p>
+    </Link>
+    <Link to="/about" className="nav-link">
+      {' '}
+      <p className="nav-text">About</p>
+    </Link>
+  </nav>
+)
+export default Header
